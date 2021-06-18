72411d9a4c8dfcf80202cb8e93b1418921cb8e92
oren.motiei <oren.motiei@mail.huji.ac.il>
2021-06-18 17:37:00 +0300

    v1.4

--- vl_vector.h
+++ vl_vector.h
@@ -1,6 +1,8 @@
 #ifndef _VL_VECTOR_H_
 #define _VL_VECTOR_H_
 
+#include <algorithm>
+
 #define DEFAULT_STATIC_CAPACITY 16
 #define GROWTH_FACTOR 1.5
 
@@ -14,6 +16,7 @@
  */
 template<typename T, const int static_cap = DEFAULT_STATIC_CAPACITY>
 class vl_vector {
+
  private: // fields
   T _stack_data[static_cap];
   T *_heap_data;
@@ -27,6 +30,40 @@ class vl_vector {
       _cap (static_cap)
   {}
 
+ public: // Iterator & ConstIterator
+  using iterator = T *;
+  using const_iterator = const T *;
+
+  iterator begin () noexcept (true)
+  {
+    return data ();
+  }
+
+  iterator end () noexcept (true)
+  {
+    return data () + _size;
+  }
+
+  const_iterator begin () const noexcept (true)
+  {
+    return begin ();
+  }
+
+  const_iterator end () const noexcept (true)
+  {
+    return end ();
+  }
+
+  const_iterator cbegin () const noexcept (true)
+  {
+    return begin ();
+  }
+
+  const_iterator cend () const noexcept (true)
+  {
+    return end ();
+  }
+
  private: // private methods
   /**
    * The capacity function that indicates the maximum amount of element
@@ -35,13 +72,37 @@ class vl_vector {
    * @param k Number of elements we want to add to a vector.
    * @return The maximal amount of elements a vector can contain.
    */
-  static size_t cap_c (const size_t &size, const size_t &k)
+  static size_t cap_c (const size_t &size, const size_t &k) noexcept (true)
   {
     return (size + k <= static_cap) ?
            static_cap : (int) (GROWTH_FACTOR * (size + k));
   }
 
  public: // public methods
+  /**
+   * @return a pointer to the variable that holds currently the data
+   *         (on the stack or on the heap).
+   */
+  T *data () noexcept (true)
+  {
+    return (_cap == static_cap) ? _stack_data : _heap_data;
+  }
+
+  /**
+   * @return a const pointer to the variable that holds currently the data
+   *         (on the stack or on the heap).
+   */
+  const T *data () const noexcept (true)
+  {
+    return (_cap == static_cap) ? _stack_data : _heap_data;
+  }
+
+  bool contains (const T &element) const noexcept (false)
+  {
+    auto result = std::find (data (), data () + _size, element);
+    return (result == data () + _size) ? false : true;
+  }
+
   /**
    * @return the current amount of elements in the vector.
    */
@@ -74,7 +135,7 @@ class vl_vector {
   {
     if (i >= _size)
       throw std::out_of_range{"Invalid index"};
-    return (_cap == static_cap) ? _stack_data[i] : _heap_data[i];
+    return data ()[i];
   }
 
   /**
@@ -85,14 +146,42 @@ class vl_vector {
   {
     if (_size + 1 <= static_cap)
       {
-        _data[_size] = element;
-        ++_size;
+        _stack_data[_size] = element;
       }
     else if (_size == _cap)
       {
-        _cap = cap_c (_size, 1);
-        T *heap_data = new
+        if (_cap == static_cap)
+          {
+            _cap = cap_c (_size, 1);
+            _heap_data = new T[_cap];
+            std::copy (_stack_data, _stack_data + _size, _heap_data);
+          }
+        else
+          {
+            _cap = cap_c (_size, 1);
+            T *temp = new T[_cap];
+            std::copy (_heap_data, _heap_data + _size, temp);
+            delete[] _heap_data;
+            _heap_data = temp;
+          }
+        _heap_data[_size] = element;
       }
+    else
+      {
+        _heap_data[_size] = element;
+      }
+    ++_size;
   }
+
+  T &operator[] (const size_t &i) noexcept (true)
+  {
+    return data ()[i];
+  }
+
+  T operator[] (const size_t &i) const noexcept (true)
+  {
+    return data ()[i];
+  }
+
 };
 #endif //_VL_VECTOR_H_
